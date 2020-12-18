d9d97374f5608a37f94c2b3ed7d9931ac48c4d02
imflop <imflop@gmail.com>
2020-12-18 23:16:16 +0300

    Filtered zero value salary items:
    - check salary in task
    - small fixes

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -10,13 +10,16 @@ def load_hh_data():
     if os.path.exists(AdvertService.get_file_path()):
         with open(AdvertService.get_file_path(), "r") as json_file:
             data = json.load(json_file)
+            count = len(data)
             for item in data:
                 service = AdvertService(item)
                 advert = service.get_advert_object()
-                advert.save()
-                advert.stack.set(*service.get_stack_list())
-                advert.save()
-            msg = "Data are saved"
+                if advert.salary_from > 0 and advert.salary_to > 0:
+                    advert.save()
+                    advert.stack.set(*service.get_stack_list())
+                    advert.save()
+                    count -= 1
+            msg = f"{count} items are saved"
     else:
-        msg = f"File on {AdvertService.get_file_path()} is not found"
+        msg = f"File by {AdvertService.get_file_path()} is not found"
     return msg
