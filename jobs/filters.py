ab958e781c3ea448ff854ad09eaaad76cb2f7e36
imflop <imflop@gmail.com>
2021-03-16 21:44:52 +0300

    bit refactoring begin

--- jobs/filters.py
+++
@@ -1,9 +0,0 @@
-from utils.filters import StyledFilterSet
-from jobs.models import Advert
-
-
-class AdvertFilter(StyledFilterSet):
-
-    class Meta:
-        model = Advert
-        fields = ('level', 'scope',)
--- remoteme/urls.py
+++ remoteme/urls.py
@@ -13,29 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
-from django.contrib.sitemaps import views as sitemaps_views
-
-from remoteme.sitemaps import AdvertSitemap
-
-
-sitemaps = {
-    'advert': AdvertSitemap
-}
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('select2/', include('django_select2.urls')),
-    path('', include('jobs.urls')),
-    path('sitemap.xml', sitemaps_views.index, {'sitemaps': sitemaps}),
-    path('sitemap-<section>.xml', sitemaps_views.sitemap, {'sitemaps': sitemaps},
-         name='django.contrib.sitemaps.views.sitemap'),
+    path("admin/", admin.site.urls),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
